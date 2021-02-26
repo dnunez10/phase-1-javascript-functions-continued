cf5be0b503ebb5092099161cb27cf1d5ef735243
dnunez10 <diana_nunez10@yahoo.com>
2021-02-26 12:55:15 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+//Your code here
+function saturdayFun(fun='roller-skate') {
+   return (`This Saturday, I want to ${fun}!`);
+}
+saturdayFun();
+saturdayFun('bathe my dog');
+
+function mondayWork(office='go to the office') {
+   return (`This Monday, I will ${office}.`)
+}
+mondayWork();
+mondayWork('work from home');
+
+function wrapAdjective(star="*") {
+   return function(inner="special") {
+     return `You are ${star}${inner}${star}!`
+   }
+ }
+ 
